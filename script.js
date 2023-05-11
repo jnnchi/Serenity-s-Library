2fb96c5db4983246c8e89ea8f08c343318e01154
Jennifer Chiou <pl247091@ahschool.com>
2023-05-10 23:22:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById("website").style.display = "none";
 function checkPassword() {
 			var password = document.getElementById("password").value;
 
+	if (password !=""){
       document.getElementById("password-form").style.display = "none";
-  document.getElementById("website").style.display = "block"; 
+  document.getElementById("website").style.display = "block"; }
 		}
