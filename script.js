5763e190302fa7dfe471113c8c3500621a4a8a2f
Jennifer Chiou <pl247091@ahschool.com>
2023-05-10 22:55:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("password-form").style.display = "block"; 
+document.getElementById("website").style.display = "none";
+
+function checkPassword() {
+			var password = document.getElementById("password").value;
+
+      document.getElementById("password-form").style.display = "none";
+  document.getElementById("website").style.display = "block"; 
+		}
